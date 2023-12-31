414212a1a3112f134950364c191c75e5ef3f733a
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2023-12-31 00:06:18 -0500

    created a function to build a syntax tree from any dreamberd expression

---
+++ syntax_tree.py
@@ -0,0 +1,188 @@
+from __future__ import annotations
+from typing import Optional, Union
+
+from base import STR_TO_OPERATOR, Token, TokenType, OperatorType, InterpretationError, VAR_DECL_KW
+
+class ListNode:
+    def __init__(self, values: list[SyntaxTreeNode]):
+        self.values = values
+
+class ExpressionNode:
+    def __init__(self, left: SyntaxTreeNode, right: SyntaxTreeNode, operator: OperatorType):
+        self.left = left 
+        self.right = right
+        self.operator = operator
+
+class FunctionNode:
+    def __init__(self, name: str, args: list[SyntaxTreeNode]):
+        self.name = name 
+        self.args = args
+
+class Value:
+    def __init__(self, name_or_value: str, index: Optional[SyntaxTreeNode] = None): 
+        self.name_or_value = name_or_value
+        self.index = index
+
+SyntaxTreeNode = Union[ExpressionNode, FunctionNode, Value, ListNode]
+
+def build_expression_tree(filename: str, tokens: list[Token]) -> SyntaxTreeNode:
+    """ 
+    This language has significant whitespace, so the biggest split happens where there is most space
+     - func a, b  +  c becomes func(a, b) + c but func a, b+c  becomes func(a, b + c) 
+     - a + func  b  ,  c + d is not legal because it translates to (a + func)(b, c + d)
+     - 2 * 1+3 becomes 2 * (1 + 3)
+    """
+
+    for token in tokens:
+        if token.type == TokenType.WHITESPACE and '\t' in token.value:
+            raise InterpretationError(filename, token.line, "Tabs are not allowed in expressions.")
+    
+    # create a new list consisting and tokens and a brand new type: the list 
+    tokens_without_whitespace = [token for token in tokens if token.type != TokenType.WHITESPACE]
+
+    # transform a list of tokens to include operators 
+    # find the operator with the maximum whitespace between it and other things
+    updated_list = [STR_TO_OPERATOR.get(token.value, token) for token in tokens]
+    max_width, max_index = 0, -1
+    for i in range(len(updated_list)):
+        if isinstance(updated_list[i], OperatorType):
+            try:
+                l_len, r_len = 0, 0
+                if tokens[i - 1].type == TokenType.WHITESPACE:
+                    l_len = len(tokens[i - 1].value)
+                if tokens[i + 1].type == TokenType.WHITESPACE:
+                    r_len = len(tokens[i + 1].value)
+                if l_len != r_len:
+                    raise InterpretationError(filename, tokens[i].line, "Whitespace must be equal on either side of an operator.")
+                if l_len >= max_width:
+                    max_width = l_len
+                    max_index = i
+            except IndexError:
+                raise InterpretationError(filename, tokens[i].line, "Operator cannot be at the end of an expression.")
+
+    # there is no operator, must be just a value
+    if max_index == -1:
+        name_or_value = tokens_without_whitespace[0]
+        if name_or_value.type != TokenType.NAME:
+            raise InterpretationError(filename, name_or_value.line, "Expected name or value.")
+        if any(l := [token.type == TokenType.L_SQUARE for token in tokens]):
+            start, end = l.index(True), len(tokens) - 1
+            while end > start:
+                if tokens[end] == TokenType.R_SQUARE:
+                    break
+                end -= 1
+            else:
+                raise InterpretationError(filename, tokens[start].line, "Excepted closing brace for index operation.")
+        return Value(name_or_value.value, tokens[start + 1 : end] if any(l) else None)  # it's not unbound trust me pyright
+        
+    # max_index is the token with the maximum surrouding whitespace 
+    if updated_list[max_index].value == ',':  
+        # this means it is a function
+        # we need to find every other comma as they become the arguments of the function
+        # additionally, there needs to be a spacing of equal length between the name of the function and the next argument
+        
+        is_valid_list = tokens_without_whitespace[0].type == TokenType.L_SQUARE
+        is_valid_func = tokens_without_whitespace[0].type == TokenType.NAME and tokens_without_whitespace[1].type == TokenType.NAME
+        if not is_valid_list and not is_valid_func:
+            raise InterpretationError(filename, tokens_without_whitespace[0].line, "Expected function call.")
+        
+        all_commas = []
+        for i in range(len(updated_list)):
+            if updated_list[i].value == ',':
+                if max_width == 0 or (tokens[i - 1].type == TokenType.WHITESPACE and len(tokens[i - 1].value)) == max_width:
+                    all_commas.append(i)
+        
+        # now can split expressions within and then call the function
+        if is_valid_func:
+            return FunctionNode(tokens_without_whitespace[0].value, [
+                build_expression_tree(filename, t) for t in [
+                    tokens[comma_index + 1:next_index] for comma_index, next_index in 
+                    zip([int(tokens[0].type == TokenType.WHITESPACE), *all_commas], [*all_commas, len(tokens)])
+                ]
+            ])
+        elif is_valid_list:
+            return ListNode([
+                build_expression_tree(filename, t) for t in [
+                    tokens[comma_index + 1:next_index] for comma_index, next_index in 
+                    zip([int(tokens[0].type == TokenType.WHITESPACE) + 1, *all_commas], 
+                        [*all_commas, len(tokens) - 1 - int(tokens[-1].type == TokenType.WHITESPACE)])  # adjusting here in order to avoid the bracket tokens
+                ]
+            ])
+
+    else: 
+        return ExpressionNode(
+            build_expression_tree(filename, tokens[:max_index]), 
+            build_expression_tree(filename, tokens[max_index + 1:]), 
+            operator=updated_list[max_index]
+        )
+
+def generate_syntax_tree(filename: str, tokens: list[Token]): 
+    """ Split the code up into lines, which are then parsed and shit """
+    curr = 0
+    current_names = {}
+    while curr < len(tokens):
+        token = tokens[curr]
+
+        if token.type == TokenType.WHITESPACE:  # this is at the start of a line or statement
+            if '\t' in token.value or len(token.value) % 3:
+                raise InterpretationError(filename, token.line, "Indentation must be with spaces and a multiple of 3.")
+
+        elif token.type in [TokenType.QUESTION, TokenType.BANG]:
+            raise InterpretationError(filename, token.line, "Ending statement before creating it.")
+
+        elif token.type != TokenType.NAME: 
+            raise InterpretationError(filename, token.line, "Excepted a name or keyword at the beginning of a statement.")
+
+        if token.value in VAR_DECL_KW:
+            modifier = []
+            
+            while token.value in VAR_DECL_KW or token.type == TokenType.WHITESPACE:
+                if token.value in VAR_DECL_KW:
+                    modifier.append(token.value)
+                curr += 1
+                token = tokens[curr]          
+        
+            names = []
+            lifetime = ''
+            while token.type != TokenType.EQUAL:
+
+                # apparently splitting in this does nothing??
+                if token.type in {TokenType.L_SQUARE, TokenType.R_SQUARE, TokenType.COMMA, TokenType.WHITESPACE}:
+                    continue
+
+                # lifetime detected in the form of <something>
+                elif token.type == TokenType.LESS_THAN:
+                    while token.type != TokenType.GREATER_THAN:  # looking for closing >
+                        if token.type == TokenType.WHITESPACE:
+                            continue
+                        elif token.type != TokenType.NAME:
+                            raise InterpretationError(filename, token.line, "Excepted name in lifetime declaration.")
+                        lifetime = token.value
+                        curr += 1
+                        token = tokens[curr] 
+                    curr += 1  
+                    token = tokens[curr]  # skip over the greater than token
+                    continue
+
+                # anything not a name is a bad token
+                elif token.type != TokenType.NAME:
+                    raise InterpretationError(filename, token.line, "Expected a name for variable declaration.")
+                names.append(token.value)
+                curr += 1
+                token = tokens[curr]
+
+            # add to all the names that are naming
+            current_names |= set(names) 
+
+            if token.value != '=':
+                raise InterpretationError(filename, token.line, "Expected a single equal sign for assignment.")
+                    
+            expression_tokens = []
+            while token.type not in [TokenType.BANG, TokenType.QUESTION]:  # evaluate the expression till the end of the line
+                expression_tokens.append(token)
+                curr += 1
+                token = tokens[curr]
+    
+            expression_root = build_expression_tree(expression_tokens)
+            # now it's either a bang or question mark
+
