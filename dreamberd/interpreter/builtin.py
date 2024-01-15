99c627822d746cfdc7f3532b8b369e46a7a296bc
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-01-14 20:03:39 -0500

    some conveniece functions + to_boolean

--- dreamberd/interpreter/builtin.py
+++ dreamberd/interpreter/builtin.py
@@ -7,6 +7,13 @@ from dreamberd.base import InterpretationError
 from dreamberd.processor.syntax_tree import CodeStatement
 
 FLOAT_TO_INT_PREC = 0.00000001
+def is_int(x: Union[float, int]) -> bool:
+    return min(x % 1, 1 - x % 1) < FLOAT_TO_INT_PREC
+
+def db_not(x: DreamberdBoolean) -> DreamberdBoolean:
+    if x.value is None:
+        return DreamberdBoolean(None)
+    return DreamberdBoolean(not x.value)
 
 class Value(metaclass=ABCMeta):
     pass
@@ -34,14 +41,14 @@ class DreamberdList(Value):
             self.create_namespace(True)  # update the length
 
         def db_list_index(index: DreamberdNumber) -> Value:
-            if min(index.value % 1, 1 - index.value % 1) > FLOAT_TO_INT_PREC:
+            if not is_int(index.value):
                 raise InterpretationError("Expected integer for list indexing.")
             elif not -1 <= index.value <= len(self.values) - 1:
                 raise InterpretationError("Indexing out of list bounds.")
             return self.values[round(index.value) - 1]
 
         def db_list_pop(index: DreamberdNumber) -> Value:
-            if min(index.value % 1, 1 - index.value % 1) > FLOAT_TO_INT_PREC:
+            if not is_int(index.value):
                 raise InterpretationError("Expected integer for list popping.")
             elif not -1 <= index.value <= len(self.values) - 1:
                 raise InterpretationError("Indexing out of list bounds.")
@@ -49,7 +56,7 @@ class DreamberdList(Value):
             return self.values.pop(round(index.value) - 1)
 
         def db_list_assign(index: DreamberdNumber, val: Value) -> None:
-            if min(index.value % 1, 1 - index.value % 1) < FLOAT_TO_INT_PREC:
+            if is_int(index.value):
                 if not -1 <= index.value <= len(self.values) - 1:
                     raise InterpretationError("Indexing out of list bounds.")
                 self.values[round(index.value) - 1] = val
@@ -179,6 +186,32 @@ KEYWORDS = {kw: Name(kw, DreamberdKeyword(kw)) for kw in ['class', 'className', 
 ##           DREAMBERD BUILTINS           ##
 ############################################
 
+# the new function does absolutely nothing, lol
+def db_new(val: Value) -> Value:
+    return val
+
+def db_map() -> DreamberdMap:
+    return DreamberdMap({})
+
+def db_to_boolean(val: Value) -> DreamberdBoolean:
+    return_bool = None
+    match val: 
+        case DreamberdString():
+            return_bool = bool(val.value.strip()) or (None if len(val.value) else False)
+        case DreamberdNumber():  # maybe if it is 0.xxx, false if it is 0, true if anything else
+            return_bool = bool(round(val.value)) or (None if abs(val.value) > FLOAT_TO_INT_PREC else False)
+        case DreamberdList():
+            return_bool = bool(val.values)
+        case DreamberdMap():
+            return_bool = bool(val.self_dict)
+        case DreamberdBoolean():
+            return_bool = val.value
+        case DreamberdUndefined():
+            return_bool = False
+        case DreamberdFunction() | DreamberdObject() | DreamberdKeyword():
+            return_bool = None  # maybe for these cause im mischevious
+    return DreamberdBoolean(return_bool)
+
 def db_to_string(val: Value) -> DreamberdString:
     return_string = str(val)
     match val:
@@ -197,6 +230,8 @@ def db_to_string(val: Value) -> DreamberdString:
             return_string = f"<object {val.class_name}>" 
         case DreamberdUndefined():
             return_string = "undefined"
+        case DreamberdKeyword():
+            return_string = val.value
     return DreamberdString(return_string)
 
 def db_print(*vals: Value) -> None:
@@ -213,6 +248,14 @@ def db_to_number(val: Value) -> DreamberdNumber:
             return_number = 0 
         case DreamberdBoolean():
             return_number = int(val.value is not None and val.value) + (val.value is None) * 0.5
+        case DreamberdList():
+            if val.values:
+                raise InterpretationError("Cannot turn a non-empty list into a number.")
+            return_number = 0 
+        case DreamberdMap():
+            if val.self_dict:
+                raise InterpretationError("Cannot turn a non-empty map into a number.")
+            return_number = 0 
         case _:
             raise InterpretationError(f"Cannot turn type {type(val).__name__} into a number.")
     return DreamberdNumber(return_number)
