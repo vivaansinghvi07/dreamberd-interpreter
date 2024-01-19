ee2a9f4487a68aa64c8d3ee3901b8174471ef7c3
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-01-19 14:13:26 -0500

    some small changes to get them out of my git status

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ import dreamberd.processor.expression_tree as db_exp_tree
 import dreamberd.processor.lexer as db_lexer
 import dreamberd.processor.syntax_tree as db_syn_tree
 
-# sys.tracebacklimit = 0
+# sys.tracebacklimit = 0  # uncomment this line for db-specific error messages only
 
 def test_expression():
-    code = "c  ^  -func [1, 2, 3][[  -4,  func 5,  other 6, 7  ][1]] ^ ;7"
+    code = "await next name"
     tokens = db_lexer.tokenize("test.db", code)
     exp = db_exp_tree.build_expression_tree("test.db", tokens, code)
     print("\n ", code, "\n")
     print(exp.to_string(), "\n")
 
-if __name__ == "__main__":
+def main():
     with open(sys.argv[1]) as f:
         code = f.read()
     tokens = db_lexer.tokenize(sys.argv[1], code)[:-1]
     tree = db_syn_tree.generate_syntax_tree(sys.argv[1], tokens, code)
     print(*tree, sep="\n")
+
+if __name__ == "__main__":
+    main()
