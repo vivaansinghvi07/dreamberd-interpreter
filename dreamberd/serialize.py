9ca09f26754e58209a5eadb69f18571a82019ce6
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-03-22 22:45:38 -0400

    finished serialization function, now need to deserialze

--- dreamberd/serialize.py
+++ dreamberd/serialize.py
@@ -1,33 +1,39 @@
 import json
 import dataclasses
-from typing import Any, Union, assert_never
-from dreamberd.base import NonFormattedError
+from typing import Any, Callable, Union, assert_never
+from dreamberd.base import NonFormattedError, Token, TokenType
 
-from dreamberd.builtin import BuiltinFunction, DreamberdFunction, DreamberdList, DreamberdNumber, DreamberdString, Name, Value, Variable
+from dreamberd.builtin import KEYWORDS, BuiltinFunction, DreamberdFunction, DreamberdList, DreamberdNumber, DreamberdString, Name, Value, Variable
+from dreamberd.interpreter import interpret_code_statements, load_globals
+from dreamberd.processor.lexer import tokenize
+from dreamberd.processor.syntax_tree import CodeStatement, VariableAssignment, generate_syntax_tree
 
 SerializedDict = dict[str, Union[str, dict, list]]
+DataclassSerializations = Union[Name, Variable, Value, CodeStatement, Token]
 
 def serialize_obj(obj: Any) -> SerializedDict:
     match obj:
-        case Name() | Variable() | Value(): return serialize_dreamberd_obj(obj)
+        case Name() | Variable() | Value() | CodeStatement() | Token(): return serialize_dreamberd_obj(obj)
         case _: return serialize_python_obj(obj)
 
 def serialize_python_obj(obj: Any) ->  dict[str, Union[str, dict, list]]:
     match obj:
+        case TokenType(): val = obj.value
         case dict(): 
             if not all(isinstance(k, str) for k in obj):
                 raise NonFormattedError("Serialization Error: Encountered non-string dictionary keys.")
             val = {k: serialize_obj(v) for k, v in obj.items()}
-        case list(): val = [serialize_obj(x) for x in obj]
+        case list() | tuple(): val = [serialize_obj(x) for x in obj]
         case str(): val = obj
         case int() | float(): val = str(obj)
+        case func if isinstance(func, Callable): val = func.__name__
         case _: assert_never(obj)
     return {
         "python_obj_type": type(obj).__name__,
         "value": val
     }
 
-def serialize_dreamberd_obj(val: Union[Value, Name, Variable]) -> dict[str, Union[str, dict, list]]:
+def serialize_dreamberd_obj(val: DataclassSerializations) -> dict[str, Union[str, dict, list]]:
     return {
         "dreamberd_obj_type": type(val).__name__,
         "attributes": [
@@ -40,9 +46,20 @@ def serialize_dreamberd_obj(val: Union[Value, Name, Variable]) -> dict[str, Unio
     }
 
 if __name__ == "__main__":
+
+    code = """f main() => {
+      print "Hello world"!
+      }"""
+    tokens = tokenize("", code)
+    statements = generate_syntax_tree("", tokens, code)
+    func_ns = KEYWORDS.copy()
+    load_globals("", "", {}, set(), [], {})
+    interpret_code_statements(statements, [func_ns], [], [{}])
+
     list_test_case = DreamberdList([
         DreamberdString("Hello world!"),
-        DreamberdNumber(123.45)
+        DreamberdNumber(123.45), 
+        func_ns["main"].value
     ])
-    __import__('pprint').pprint(json.dumps(serialize_obj(list_test_case)))
+    __import__('pprint').pprint(serialize_obj(list_test_case))
         
